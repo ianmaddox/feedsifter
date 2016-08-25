53038a10491eb22b833a76a26eaf695e3678cd49
Ian Maddox <ianmaddox@users.noreply.github.com>
2016-08-25 03:37:51 -0700

    CORS FTW

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.set('port', (process.env.PORT || 5000));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.get('/', function(req, res) {
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   if(req.query.f !== undefined) {
